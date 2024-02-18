c314d601c2df24742df1b3010d2fe186b51c1647
ibuki.takahashi <ibuki.takahashi.sw@ambl.co.jp>
2024-02-18 16:01:07 +0900

    update: LandingPage(Home)の追加

--- dutch/src/App.tsx
+++ dutch/src/App.tsx
@@ -1,16 +1,21 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Content from './Content'
+import { Header } from './components/Header'
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>React-router-V6</h1>
-      <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/content" element={ <Content /> } />
-      </Routes>
+    <div className="App h-screen grid grid-rows-10">
+      <div className='my-auto row-span-1'>
+        <Header children='飲み会割り勘アプリ'></Header>
+      </div>
+      <div className='row-span-9'>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/content" element={ <Content /> } />
+        </Routes>
+      </div>
     </div>
   )
 }
